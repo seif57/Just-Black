4ae19aac38bc7b35ee25f8f3fd2bd9cc2de1e1f9
seif <seif@outlook.com>
2022-01-22 13:46:34 +0200

    added an collectionOverview Component and moved the shop data and directory data to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import persistReducer from "redux-persist/es/persistReducer";
 import storage from "redux-persist/lib/storage";
 
+import shopReducer from "./shop/shop.reducer";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
   key: "root",
@@ -14,6 +16,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
